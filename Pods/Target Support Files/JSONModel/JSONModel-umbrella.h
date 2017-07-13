dd4d72da24450ff398b73a9a7a6d4228eabb4ecc
pengchao <hunter858@sina.cn>
2017-07-13 19:26:40 +0800

    delete .gitignore

---
+++ Pods/Target Support Files/JSONModel/JSONModel-umbrella.h
@@ -0,0 +1,15 @@
+#import <UIKit/UIKit.h>
+
+#import "JSONModel.h"
+#import "JSONModelClassProperty.h"
+#import "JSONModelError.h"
+#import "JSONModelLib.h"
+#import "JSONAPI.h"
+#import "JSONHTTPClient.h"
+#import "JSONModel+networking.h"
+#import "JSONKeyMapper.h"
+#import "JSONValueTransformer.h"
+
+FOUNDATION_EXPORT double JSONModelVersionNumber;
+FOUNDATION_EXPORT const unsigned char JSONModelVersionString[];
+
